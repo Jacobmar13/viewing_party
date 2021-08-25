b3346779ac0180fc93e82def696a8a557beda64b
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-25 17:20:37 -0600

    Law abiding citizen

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,4 @@
 class Guest < ApplicationRecord
-  belongs_to :user, foreign_key: 'guest_id'
-  belongs_to :viewing_party, foreign_key: 'party_id'
+  belongs_to :user, foreign_key: 'guest_id', inverse_of: :guests
+  belongs_to :viewing_party, foreign_key: 'party_id', inverse_of: :guests
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :friends
-  has_many :friends, foreign_key: 'friend_id'
-  has_many :viewing_parties, foreign_key: 'organizer_id'
-  has_many :guests, foreign_key: 'guest_id'
+  has_many :friends, dependent: nil
+  has_many :friends, foreign_key: 'friend_id', inverse_of: :user, dependent: nil
+  has_many :viewing_parties, foreign_key: 'organizer_id', inverse_of: :user, dependent: nil
+  has_many :guests, foreign_key: 'guest_id', inverse_of: :user, dependent: nil
 end
