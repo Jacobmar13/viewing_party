7192472ae5d76a7efcc06b33d254aa752062d5c0
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-25 17:10:41 -0600

    Law abiding citizen

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :friends
-  has_many :friends, foreign_key: "friend_id"
-  has_many :viewing_parties, foreign_key: "organizer_id"
-  has_many :guests, foreign_key: "guest_id"
+  has_many :friends, foreign_key: 'friend_id'
+  has_many :viewing_parties, foreign_key: 'organizer_id'
+  has_many :guests, foreign_key: 'guest_id'
 end
