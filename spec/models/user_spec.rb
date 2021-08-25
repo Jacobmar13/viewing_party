d3898e806de0013ad3c4ee48431f5ba92a7a6f9f
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-25 16:52:39 -0600

    Add one more assocation test for friend_id column

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User do
   describe 'relationships' do
     it { should have_many(:friends)}
+    it { should have_many(:friends).with_foreign_key('friend_id')}
     it { should have_many(:viewing_parties).with_foreign_key('organizer_id') }
     it { should have_many(:guests).with_foreign_key('guest_id')}
   end
