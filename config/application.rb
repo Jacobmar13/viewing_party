77a5ee0ba826c66b7ec33b3ad55993b6bd9c78e6
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-25 16:48:56 -0600

    Changed module name to deal with model conflict

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ViewingParty
+# Changed from ViewingParty
+module Party
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
