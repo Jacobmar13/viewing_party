705021b82d904a791030208bf1a443fe5cf7563e
Brett Jones <bhjones4545@gmail.com>
2021-08-26 17:07:48 -0600

    Refactor dashboard tests

--- spec/features/user/show_spec.rb
+++ spec/features/user/show_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 RSpec.describe 'user show page' do
-  it 'can not access show page without login' do
-    visit user_path
-    expect(current_path).to_not eq(user_path(user.id))
-    expect(current_path).to eq(root_path)
+  describe 'not authenticated user' do
+    it 'can see 400 error when not logged in' do
+      visit user_path(1)
+
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
+
+  describe 'as authenticated user' do
+    it 'can have a dashboard page' do
+        user = User.create(email: 'hello@example.com', password: '1234')
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+        visit user_path(user.id)
+        
+        expect(page).to have_content("Welcome #{user.email}!")
+    end
   end
 end
