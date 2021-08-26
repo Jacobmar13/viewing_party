b75196c0f5a33c05a312584ab299503b19a3afc4
Brett Jones <bhjones4545@gmail.com>
2021-08-26 15:00:46 -0600

    Add controller methods new/create for registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,32 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = user_params
+    user[:email] = user[:email].downcase
+    if user_params[:password] == user_params[:password_confirmation]
+      new_user = User.create(user_params)
+      flash[:success] = "Registration successful. Please log in!"
+      redirect_to root_path
+    else
+      flash[:error] = "Passwords do not match!"
+      redirect_to new_user_path
+    end
+  end
+
   def show
     binding.pry
   end
 
   def authenticate
     redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
