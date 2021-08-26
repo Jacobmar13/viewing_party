808feef499bc1104d5f4e61647dec4f3d452c68b
Brett Jones <bhjones4545@gmail.com>
2021-08-26 15:00:09 -0600

    Add Bcrypt method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :friends, foreign_key: 'friend_id', inverse_of: :user, dependent: nil
   has_many :viewing_parties, foreign_key: 'organizer_id', inverse_of: :user, dependent: nil
   has_many :guests, foreign_key: 'guest_id', inverse_of: :user, dependent: nil
+
+  has_secure_password
 end
