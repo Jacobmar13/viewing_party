fb62df45823001dfcccb84f9f2030dbfd0197c25
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-27 14:41:06 -0600

    Refactor relationships

--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Friend do
   describe 'associations' do
     it { should belong_to(:user) }
-    it { should belong_to(:user).with_foreign_key('friend_id') }
+    it { should belong_to(:each_friend) }
   end
 end
