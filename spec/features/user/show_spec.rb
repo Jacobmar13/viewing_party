410edf803bda4d88dbcd69cad919b4e78ff113c0
Brett Jones <bhjones4545@gmail.com>
2021-08-26 21:01:06 -0600

    Create display friends test

--- spec/features/user/show_spec.rb
+++ spec/features/user/show_spec.rb
@@ -10,31 +10,71 @@ RSpec.describe 'user show page' do
 
   describe 'as authenticated user' do
     it 'can have a dashboard page' do
-        user = User.create(email: 'hello@example.com', password: '1234')
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-        visit user_path(user.id)
+      user = User.create(email: 'hello@example.com', password: '1234')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit user_path(user.id)
 
-        expect(page).to have_content("Welcome #{user.email}!")
+      expect(page).to have_content("Welcome #{user.email}!")
     end
   end
 
   describe 'as authenticated user cannot visit another users show page' do
     it 'cannot access another users show page' do
-        user = User.create(email: 'hello@example.com', password: '1234')
-        user_2 = User.create(email: 'goodbye@example.com', password: 'abcd')
+      user = User.create(email: 'hello@example.com', password: '1234')
+      user_2 = User.create(email: 'goodbye@example.com', password: 'abcd')
 
-        visit root_path
+      visit root_path
 
-        fill_in 'email', with: 'hello@example.com'
-        fill_in 'password', with: '1234'
+      fill_in 'email', with: 'hello@example.com'
+      fill_in 'password', with: '1234'
 
-        click_on 'Sign In'
+      click_on 'Sign In'
 
-        expect(current_path).to eq(user_path(user.id))
+      expect(current_path).to eq(user_path(user.id))
 
-        visit user_path(user_2.id)
-        
-        expect(page).to have_content("The page you were looking for doesn't exist.")
+      visit user_path(user_2.id)
+
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+    end
+  end
+
+  describe 'has friends section' do
+    it 'displays string when user has no friends' do
+      user = User.create(email: 'hello@example.com', password: '1234')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit user_path(user.id)
+      expect(page).to have_content("You currently have no friends")
+    end
+
+    xit "displays friend's emails if friends are added" do
+      user = User.create(email: 'hello@example.com', password: '1234')
+      user_2 = User.create(email: 'goodbye@example.com', password: 'abcd')
+      user_3 = User.create(email: 'thankyou@example.com', password: '4321')
+
+      Friend.create(user_id: user.id, friend_id: user_2.id)
+      Friend.create(user_id: user.id, friend_id: user_3.id)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit user_path(user.id)
+
+      expect(page).to have_content(user_2.email)
+      expect(page).to have_content(user_3.email)
+    end
+
+    it 'can add friends by form' do
+      user = User.create(email: 'hello@example.com', password: '1234')
+      user_2 = User.create(email: 'goodbye@example.com', password: 'abcd')
+      user_3 = User.create(email: 'thankyou@example.com', password: '4321')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit user_path(user.id)
+
+      fill_in :email, with: user_2.email
+      click_on 'Add Friend'
+
+      expect(page).to have_content(user_2.email)
     end
   end
 end
