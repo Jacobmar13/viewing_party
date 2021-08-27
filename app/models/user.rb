d89c1c6cb90e7ce28514a74518656ec03065bee7
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-27 14:30:40 -0600

    Change how associations are structured

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true
-  
+
   has_many :friends, dependent: nil
-  has_many :friends, foreign_key: 'friend_id', inverse_of: :user, dependent: nil
+  has_many :each_friend, through: :friends
   has_many :viewing_parties, foreign_key: 'organizer_id', inverse_of: :user, dependent: nil
   has_many :guests, foreign_key: 'guest_id', inverse_of: :user, dependent: nil
 
   has_secure_password
+
+  def friend_info
+    self.friends.map do |friend|
+      User.where(id: friend.friend_id)
+    end.flatten
+  end
 end
