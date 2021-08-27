9b11acce0a46d961580389ccf214b8ab89da0170
Brett Jones <bhjones4545@gmail.com>
2021-08-26 18:01:21 -0600

    remove snop

--- spec/features/sessions/create_spec.rb
+++ spec/features/sessions/create_spec.rb
@@ -4,12 +4,11 @@ RSpec.describe 'Create user session' do
     user = User.create(email: 'hello@example.com', password: '1234')
 
     visit root_path
-    
+
     fill_in 'email', with: 'hello@example.com'
     fill_in 'password', with: '1234'
 
     click_on 'Sign In'
-save_and_open_page
     expect(current_path).to eq(user_path(user.id))
   end
 end
