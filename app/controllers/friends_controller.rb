903816cb053c0f837d5090cd17b43008d297c453
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-27 14:29:53 -0600

    Remove variable

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,7 @@
 class FriendsController < ApplicationController
   def create
     friend = User.find_by(email: params[:email])
-    x = Friend.create!(user_id: params[:user_id], friend_id: friend.id)
+    Friend.create!(user_id: params[:user_id], friend_id: friend.id)
     redirect_to user_path(params[:user_id])
   end
 end
