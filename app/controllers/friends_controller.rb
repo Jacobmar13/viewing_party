2b5a2c4f26f9db8715861d65d4fc45c181aef74e
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-29 20:45:57 -0600

    Law abiding citizen

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,10 +3,9 @@ class FriendsController < ApplicationController
     if User.exists?(email: params[:email])
       friend = User.find_by(email: params[:email])
       Friend.create!(user_id: params[:user_id], friend_id: friend.id)
-      redirect_to user_path(params[:user_id])
     else
       flash[:error] = "I'm sorry, your friend cannot be found."
-      redirect_to user_path(params[:user_id])
     end
+    redirect_to user_path(params[:user_id])
   end
 end
