244aab1a844e25e185920f99663df9b4d4072cc4
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-29 20:40:37 -0600

    Add sad path for friend not found

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,12 @@
 class FriendsController < ApplicationController
   def create
-    friend = User.find_by(email: params[:email])
-    Friend.create!(user_id: params[:user_id], friend_id: friend.id)
-    redirect_to user_path(params[:user_id])
+    if User.exists?(email: params[:email])
+      friend = User.find_by(email: params[:email])
+      Friend.create!(user_id: params[:user_id], friend_id: friend.id)
+      redirect_to user_path(params[:user_id])
+    else
+      flash[:error] = "I'm sorry, your friend cannot be found."
+      redirect_to user_path(params[:user_id])
+    end
   end
 end
