28c641c4e44d463a9ed5b80404bac17066da6e2e
Brett Jones <bhjones4545@gmail.com>
2021-08-31 14:56:55 -0600

    Create test for dashboard instantiation

--- spec/features/movies/discover_spec.rb
+++ spec/features/movies/discover_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
-RSpec.describe 'filter by top 40 movie results' do
-  it 'can display top 40 movies by vote average' do
-    
+RSpec.describe 'Discover Page' do
+  describe 'instantiation' do
+    it 'can show button and form' do
+      user = User.create(email: 'hello@example.com', password: '1234')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit discover_path
+
+      expect(page).to have_content("Welcome #{user.email}!")
+      expect(page).to have_button("Find Top Rated Movies")
+      expect(page).to have_field(:filter)
+      expect(page).to have_button("Find Movies")
+    end
   end
 end
