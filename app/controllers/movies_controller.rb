05ef023a4b99a094a32ee217583f0874aeb3e115
Brett Jones <bhjones4545@gmail.com>
2021-08-31 15:00:55 -0600

    Add discover and show methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
-  def discover; end
+  before_action :require_login
+
+  def discover
+    @user = current_user
+  end
 
   def movies
     if params[:filter].nil?
@@ -7,5 +11,12 @@ class MoviesController < ApplicationController
     elsif params[:filter].present?
       @movies = MovieFacade.search_by_title(params[:filter])
     end
+  end
+
+  def show
+    @user = current_user
+    @movie = MovieFacade.movie_info_by_id(params[:movie_id])
+    @movie_cast = MovieFacade.movie_cast(params[:movie_id])
+    @movie_reviews = MovieFacade.movie_reviews(params[:movie_id])
   end
 end
