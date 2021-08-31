6c26c2dbd34a8f2cf1b039f34ee90b678208e558
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-31 16:29:07 -0600

    Remove unused code

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,8 +1,4 @@
 class MovieService
-  # def self.top_movies
-  #   get_api_data('3/movie/top_rated?language=en-US&page=1&region=US') + get_api_data('3/movie/top_rated?language=en-US&page=2&region=US')
-  # end
-
   def get_api_data(uri)
     conn = Faraday.get("https://api.themoviedb.org/#{uri}") do |req|
       req.headers['Authorization'] = ENV['TMDB_API_KEY']
