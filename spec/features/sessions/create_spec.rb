f7f9f619805bbd7500dfe60d5ebc4193e05d04bc
Brett Jones <bhjones4545@gmail.com>
2021-08-31 15:17:27 -0600

    Create sad path test for login

--- spec/features/sessions/create_spec.rb
+++ spec/features/sessions/create_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe 'Create user session' do
     click_on 'Sign In'
     expect(current_path).to eq(user_path(user.id))
   end
+
+  it 'can return error message with incorrect login info' do
+    user = User.create(email: 'hello@example.com', password: '1234')
+
+    visit root_path
+
+    fill_in 'email', with: 'hello@example.com'
+    fill_in 'password', with: 'iamwrong'
+
+    click_on 'Sign In'
+    expect(page).to have_content('Credentials are bad.')
+    expect(current_path).to eq(root_path)
+  end
 end
