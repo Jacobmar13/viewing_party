1ab21bbd4f8e448072ee2383275dcb605d91de97
Jacob Martinez <jacobmarcareer@gmail.com>
2021-08-30 20:19:44 -0600

    Add missing end

--- app/poros/movie_details.rb
+++ app/poros/movie_details.rb
@@ -2,10 +2,11 @@ class MovieDetails
   attr_reader :id,
               :title,
               :vote_average
-              
+
   def initialize(data)
     # not needed but may come in handy to know movie id given from api
     @id = data[:id]
     @title = data[:title]
     @vote_average = data[:vote_average]
+  end
 end
