33f4a8e2fcc9100f9ab04695e4a5112b1fc8b1cc
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-01 15:58:27 -0600

    Add tests for new party creation link

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Movie details page' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit movie_path(320288)
-      
+
       expect(current_path).to eq(movie_path(320288))
       expect(page).to have_content("Vote Average:")
       expect(page).to have_content("Runtime:")
@@ -15,6 +15,22 @@ RSpec.describe 'Movie details page' do
       expect(page).to have_content("Summary:")
       expect(page).to have_content("Cast:")
       expect(page).to have_content("Reviews:")
+    end
+  end
+
+  describe 'interactable elements', :vcr do
+    it 'has a link to create new viewing party' do
+      user = User.create(email: 'hello@example.com', password: '1234')
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit movie_path(320288)
+
+      expect(page).to have_button('Create Viewing Party for Movie')
+
+      click_on 'Create Viewing Party for Movie'
+
+      expect(current_path).to eq(viewing_party_new_path)
     end
   end
 end
