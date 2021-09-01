1306dee6f574d88afe5ee35eae3a6f18cac1891b
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-01 15:55:53 -0600

    Add validations for columns

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,4 +1,8 @@
 class ViewingParty < ApplicationRecord
   has_many :guests, foreign_key: 'party_id', inverse_of: :viewing_party, dependent: nil
   belongs_to :user, foreign_key: 'organizer_id', inverse_of: :viewing_parties
+
+  validates :duration, presence: true
+  validates :start_time, presence: true
+  validates :date, presence: true
 end
