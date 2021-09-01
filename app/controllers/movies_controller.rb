a04de6f3d04f71839d27ec1076c9b2214daabbc6
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-01 15:54:44 -0600

    Clean up current user in views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,6 @@ class MoviesController < ApplicationController
   before_action :require_login
 
   def discover
-    @user = current_user
   end
 
   def movies
@@ -14,9 +13,9 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @user = current_user
     @movie = MovieFacade.movie_info_by_id(params[:movie_id])
     @movie_cast = MovieFacade.movie_cast(params[:movie_id])
     @movie_reviews = MovieFacade.movie_reviews(params[:movie_id])
+    session[:movie_details] = {movie_id: params[:movie_id], duration: @movie.runtime, movie_title: @movie.title}
   end
 end
