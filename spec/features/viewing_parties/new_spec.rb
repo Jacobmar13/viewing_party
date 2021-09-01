c0782736cf4a1ae91b713da7ddfbb404d5969d67
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-01 17:23:22 -0600

    Add new tests for additions

--- spec/features/viewing_parties/new_spec.rb
+++ spec/features/viewing_parties/new_spec.rb
@@ -10,16 +10,64 @@ RSpec.describe 'Viewing party new' do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
   end
 
-  describe 'Page has a form', :vcr do
-    it 'has elements' do
-      visit viewing_party_new_path
+  describe 'Page form', :vcr do
+    it 'has viewable elements' do
+      visit movie_path(630566)
 
+      click_button 'Create Viewing Party for Movie'
+
+      expect(current_path).to eq(viewing_party_new_path)
       expect(page).to have_content('Movie Title')
       expect(page).to have_content('Duration of Party')
       expect(page).to have_content('Day')
       expect(page).to have_content('Start Time')
       expect(page).to have_button('Create Party')
     end
-  end
 
+    describe 'when filled' do
+      it 'can create party and go to dashboard on happy path' do
+        visit movie_path(630566)
+
+        click_button 'Create Viewing Party for Movie'
+
+        fill_in :duration, with: 140
+        fill_in :date, with: '2021-09-08'
+        fill_in :start_time, with: '15:23:00'
+        check 'goodbye@example.com'
+        check 'thankyou@example.com'
+        click_on 'Create Party'
+
+        expect(current_path).to eq(user_path(@user.id))
+      end
+
+      it 'can be redirected to form for too small duration on sad path' do
+        visit movie_path(630566)
+
+        click_button 'Create Viewing Party for Movie'
+
+        fill_in :duration, with: 100
+        fill_in :date, with: '2021-09-08'
+        fill_in :start_time, with: '15:23:00'
+        check 'goodbye@example.com'
+        check 'thankyou@example.com'
+        click_on 'Create Party'
+
+        expect(current_path).to eq(viewing_party_new_path)
+      end
+
+      it 'can be redirected to form for blank entries on sad path' do
+        visit movie_path(630566)
+
+        click_button 'Create Viewing Party for Movie'
+
+        fill_in :duration, with: 140
+        fill_in :start_time, with: '15:23:00'
+        check 'goodbye@example.com'
+        check 'thankyou@example.com'
+        click_on 'Create Party'
+
+        expect(current_path).to eq(viewing_party_new_path)
+      end
+    end
+  end
 end
--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe ViewingParty do
     it { should validate_presence_of(:start_time)}
     it { should validate_presence_of(:date)}
   end
+
+  describe 'methods', :vcr do
+    describe '::movie_title' do
+      it 'returns movie title from movie_id' do
+        user1 = User.create(email: 'hello@example.com', password: '1234')
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+
+        params1 = {organizer_id: user1.id, duration: 140, date: '2021-09-08', start_time: '15:23:00', movie_id: 630566}
+
+        party1 = ViewingParty.create!(params1)
+
+        expect(party1.movie_title).to eq('Clouds')
+      end
+    end
+  end
 end
