89beb4b3f006b277526d8a9373dc3883d5b07634
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-01 16:00:52 -0600

    Add validations for column entries

--- spec/models/viewing_party_spec.rb
+++ spec/models/viewing_party_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe ViewingParty do
     it { should have_many(:guests).with_foreign_key('party_id') }
     it { should belong_to(:user).with_foreign_key('organizer_id') }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:duration)}
+    it { should validate_presence_of(:start_time)}
+    it { should validate_presence_of(:date)}
+  end
 end
